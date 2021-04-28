260f35218e1db8c28b0c541d68a7daa20c564b2c
Pradas137 <pradas137@gmail.com>
2021-04-28 14:45:24 +0200

    models

---
+++ app/Models/Gamelist.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gamelist extends Model
+{
+    use HasFactory;
+}
