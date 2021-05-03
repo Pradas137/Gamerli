2f112bd72c1223da458e83121b1a4c7edd086e09
Pradas137 <pradas137@gmail.com>
2021-05-03 15:50:48 +0200

    nuevo

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="flex justify-around">
+            <a href="/admin/dashboard/terms" class="mx-3 bg-mtr-dark p-5 w-4/12 text-center font-extrabold rounded-sm text-base">VIEW TERMS</a>
+            <a href="/admin/dashboard/students" class="mx-3 bg-mtr-dark p-5 w-4/12 text-center font-extrabold rounded-sm text-base">VIEW ALUMNS</a>
+        </div>
+    </div>
+    <script src="{{asset('js/breadcrumb.js')}}"></script>
+</x-app-layout>
