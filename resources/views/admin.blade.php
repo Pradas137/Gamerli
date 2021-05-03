2a2f5b4473a1417b81c35e7fbfe8c6d4616c64a7
Pradas137 <pradas137@gmail.com>
2021-05-03 21:33:41 +0200

    nuevo admin blade

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,5 +5,4 @@
             <a href="/admin/dashboard/students" class="mx-3 bg-mtr-dark p-5 w-4/12 text-center font-extrabold rounded-sm text-base">VIEW ALUMNS</a>
         </div>
     </div>
-    <script src="{{asset('js/breadcrumb.js')}}"></script>
 </x-app-layout>
