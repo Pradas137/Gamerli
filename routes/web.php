5aaee3bba87b7fa3695fb4d0b578ea9b176e22db
Pradas137 <pradas137@gmail.com>
2021-05-04 20:53:28 +0200

    blades

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Friend;
+use App\Models\Ranking;
+use App\Models\Liste;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,3 +46,32 @@ Route::get('/admin', function () {
 Route::get('/admin/dashboard', function () {
     return view('admin');
 })->middleware(['auth',  'can:accessAdmin'])->name('dashboard');
+
+Route::get('/admin/dashboard/requests', function () {
+    $data = Request::all();
+    $user = auth::id();
+    return view('request', ['request' => $data]);
+})->middleware(['auth',  'can:accessAdmin'])->name('request');
+
+Route::get('/admin/dashboard/friends', function () {
+    $data = Friend::all();
+    $user = auth::id();
+    return view('friendAdmin', ['friends' => $data]);
+})->middleware(['auth',  'can:accessAdmin'])->name('friends');
+
+Route::get('/admin/dashboard/rankings', function () {
+    $data = Ranking::all();
+    $user = auth::id();
+    return view('rankingAdmin', ['ranking' => $data]);
+})->middleware(['auth',  'can:accessAdmin'])->name('ranking');
+
+Route::get('/admin/dashboard/lists', function () {
+    $data = Liste::all();
+    $user = auth::id();
+    return view('listsAdmin', ['lists' => $data]);
+})->middleware(['auth',  'can:accessAdmin'])->name('lists');
+
+Route::get('/admin/dashboard/profiles', function () {
+    $user = auth::id();
+    return view('profileAdmin', ['profiles' => $data]);
+})->middleware(['auth',  'can:accessAdmin'])->name('profiles');
