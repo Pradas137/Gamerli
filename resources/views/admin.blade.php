06da5a21b23acba6c1943969f1cf7cc609afa230
Pradas137 <pradas137@gmail.com>
2021-05-17 17:57:42 +0200

    sddss

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,7 +4,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="grid grid-rows-2 grid-flow-col gap-8">
-            <a href="/admin/dashboard/requests" class="bg-red-500 text-center font-extrabold rounded-sm text-base rounded-lg m-5 h-10"> Requests</a>
+            <a href="/admin/dashboard/requestss" class="bg-red-500 text-center font-extrabold rounded-sm text-base rounded-lg m-5 h-10"> Requests</a>
             <a href="/admin/dashboard/ranking" class="bg-red-500 text-center font-extrabold rounded-sm text-base rounded-lg m-5 h-10"> Rankings</a>
             <a href="/admin/dashboard/profile" class="bg-red-500 text-center font-extrabold rounded-sm text-base rounded-lg m-5 h-10"> Profile</a>
             <a href="/admin/dashboard/lists" class="bg-red-500  text-center font-extrabold rounded-sm text-base rounded-lg m-5 h-10"> Lists</a>
