92cb02983f68f1642abba85f65bd6f5dffbd75ee
Pradas137 <pradas137@gmail.com>
2021-05-19 22:08:26 +0200

    colores

--- resources/views/showpublic.blade.php
+++ resources/views/showpublic.blade.php
@@ -40,7 +40,7 @@
     @else
     <script src="{{asset('js/breadcrumb.js')}}"></script>
     <div class="flex h-screen">
-        <div class="bg-red-400 h-8 md:h-full border-r w-64 text-center border-gray-200">
+        <div class="bg-gray-400 h-8 md:h-full border-r w-64 text-center border-gray-200">
             <h6 class="font-bold mb-4 text-2xl">PAGE</h6>
             <ul>
                 <li class="font-bolt mb-4"><a href="/dashboard">Home</a></li>
