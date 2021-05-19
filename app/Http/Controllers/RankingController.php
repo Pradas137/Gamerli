987425a2470cb4306885bf4954eddf87281c1a18
kokodemo <xnominatorxx@gmail.com>
2021-05-20 01:18:46 +0200

    Update RankingController.php

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -100,13 +100,13 @@ return response()->json($data);
     {
         $ranking = DB::table('platforms')
             ->join('games', 'platforms.id', '=', 'games.platform_id')
-            ->select('platforms.*', 'games.platform_id')->groupBy('name')->get();
+            ->select('platforms.*', 'games.platform_id')->get();
             //return view('ranking.create', ['ranking' => $ranking]);
             
         $genres = DB::table('games')
             ->join('game_genre', 'games.id', '=', 'game_genre.game_id')
             ->join('genres','genres.id','=','game_genre.genre_id')
-            ->select('genres.*', 'game_genre.genre_id')->groupBy('name')->get();
+            ->select('genres.*', 'game_genre.genre_id')->get();
             return view('ranking.create', ['ranking' => $ranking,'genres' => $genres]);    
 
             
